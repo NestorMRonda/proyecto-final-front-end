bd4b3bf567a5fa91af5ae213ff5227a58be6520a
Daniel <aristaoeste4@gmail.com>
2022-12-08 18:12:53 +0100

    creacion de rutas: vacia apunta a componente landinPage home: apunta a landingPageComponent login: apunta a componente studentPrivateComponent profile/ students: apunta a componente studentsPrivateComponent form/teacher: apunta a componente teacherSingUpComp list/teacher. apunta a comp teacherListComp profile/teacher: apunta a comp teacherPrivateComp teacher/: teacherId. apunta a comp teacherDetailComp Creacion del Html del Nav Agregamos estilos al nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { StudentSingUpComponent } from './components/student-sing-up/student-sing-up.component';
+import { StudentsPrivateComponent } from './components/students-private/students-private.component';
+import { TeacherDetailsComponent } from './components/teacher-details/teacher-details.component';
+import { TeacherListComponent } from './components/teacher-list/teacher-list.component';
+import { TeacherPrivateComponent } from './components/teacher-private/teacher-private.component';
+import { TeacherSingUpComponent } from './components/teacher-sing-up/teacher-sing-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //rutas estudiantes
+  { path: '', component: LandingPageComponent },
+  { path: 'home', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'form/students', component: StudentSingUpComponent },
+  { path: 'profile/students', component: StudentsPrivateComponent },
+  //rutas teachers
+  { path: 'form/teacher', component: TeacherSingUpComponent },
+  { path: 'list/teacher', component: TeacherListComponent },
+  { path: 'profile/teacher', component: TeacherPrivateComponent },
+  { path: 'teacher/:teacherId', component: TeacherDetailsComponent },
+  { path: '**', redirectTo: 'home' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
