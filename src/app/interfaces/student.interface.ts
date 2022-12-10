5fe1c79facb135b410f27a017fb7484f5d70d1cb
Daniel <aristaoeste4@gmail.com>
2022-12-10 12:45:45 +0100

    creacion de interfaces

---
+++ src/app/interfaces/student.interface.ts
@@ -0,0 +1,10 @@
+export interface Student {
+    name: string,
+    surname: string,
+    birthdate: Date,
+    email: string,
+    password: string,
+    phone: string,
+    avatar: string,
+    type: string
+}
