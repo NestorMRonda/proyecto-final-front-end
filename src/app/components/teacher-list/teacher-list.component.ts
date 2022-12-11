347082994322d4ae0516936beaaddb40f2559c93
Javier Agudo <jagudox@gmail.com>
2022-12-11 14:13:17 +0100

    Maquetación del componente de list-teachers, fata toda la funcionalidad. y hacer que cada teacher redirija a un teacher-detail.

--- src/app/components/teacher-list/teacher-list.component.ts
+++ src/app/components/teacher-list/teacher-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SubjectsService } from 'src/app/services/subjects.service';
+import { TeachersService } from 'src/app/services/teachers.service';
 
 @Component({
   selector: 'app-teacher-list',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./teacher-list.component.css']
 })
 export class TeacherListComponent {
+  price: number
+  score: number
 
+  showScore: boolean
+  showPrice: boolean
+
+  arrTeachers: any[]
+  arrSubjects: any[]
+
+  constructor(private teacherService: TeachersService, private subjectsService: SubjectsService) {
+    this.arrTeachers = [];
+    this.arrSubjects = [];
+    this.price = 0;
+    this.score = 0;
+
+    this.showPrice = false;
+    this.showScore = false;
+
+  }
+
+  async ngOnInit() {
+    this.arrTeachers = await this.teacherService.getAll()
+    this.arrSubjects = await this.subjectsService.getAll()
+  }
+
+  changeDisplayScore() {
+    console.log(this.showScore)
+    this.showScore = !this.showScore;
+  }
+
+  changeDisplayPrice() {
+    this.showPrice = !this.showPrice;
+  }
 }
