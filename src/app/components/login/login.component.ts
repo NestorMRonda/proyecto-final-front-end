0ce9b345d1273dce127f1a141e8856a17f8bec91
Javier Agudo <jagudox@gmail.com>
2022-12-12 16:31:33 +0100

    Formularios de login y registro de students, modificación de algunos estilos de los profesores.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  formulario: FormGroup;
+
+  constructor() {
+    this.formulario = new FormGroup({
+      email: new FormControl('pepito@gmail.com', [Validators.required]),
+      password: new FormControl('test1234', [Validators.required])
+      /* Habría que ver si el email y la password coinciden con la base de datos*/
+    })
+
+  }
+
+  onSubmit() {
+    console.log(this.formulario.value)
+    this.formulario.reset()
+  }
+
+  checkError(campo: string, error: string): boolean | undefined {
+    return this.formulario.get(campo)?.hasError(error) && this.formulario.get(campo)?.touched
+  }
 }
--- src/app/components/student-sing-up/student-sing-up.component.ts
+++ src/app/components/student-sing-up/student-sing-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student-sing-up',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class StudentSingUpComponent {
 
+  formulario: FormGroup
+  regExp: RegExp
+  emailRegExp: RegExp
+
+  constructor() {
+
+    this.emailRegExp = new RegExp(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/)
+
+    this.regExp = new RegExp(/^[0-9]\d{0,2}$/)
+
+
+    this.formulario = new FormGroup({
+
+      name: new FormControl('Javie', [Validators.required]),
+      surname: new FormControl('Agudo', [Validators.required]),
+      birthdate: new FormControl(Date, [Validators.required]),
+      email: new FormControl('pepito@gmail.com', [Validators.required, Validators.pattern(this.emailRegExp)]),
+      password: new FormControl('test1234', [Validators.required, Validators.pattern(this.regExp)]),
+      repitePassword: new FormControl('test1234', [Validators.required, Validators.pattern(this.regExp)]),
+      phone: new FormControl('655875404', [Validators.required, Validators.minLength(9)]),
+      image: new FormControl('')
+
+    })
+  }
+
+  onSubmit() {
+    this.formulario.value.type = 'user'
+    console.log(this.formulario.value)
+    this.formulario.reset()
+  }
+
+  checkError(campo: string, error: string): boolean | undefined {
+    return this.formulario.get(campo)?.hasError(error) && this.formulario.get(campo)?.touched
+  }
 }
