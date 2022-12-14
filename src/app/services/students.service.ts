0295b0b6fdc8b85df377fbb21a5b8cfcb3e6d27e
Javier Agudo <jagudox@gmail.com>
2022-12-14 15:56:21 +0100

    Registro y logIn casi terminados, meter libreria sweet alert y hacer navigate a alumno o teacher, guardar en el localStorage el token.

--- src/app/services/students.service.ts
+++ src/app/services/students.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { Student } from '../interfaces/student.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,16 @@ export class StudentsService {
   baseUrl: string;
 
   constructor(private httpClient: HttpClient) {
-    this.baseUrl = ''
+    this.baseUrl = 'http://localhost:3000/api/users'
 
   }
 
+
+  logIn(pBody: any) {
+    return firstValueFrom(
+      this.httpClient.post<any>(`${this.baseUrl}/login`, pBody)
+    )
+  }
 
 
 }
