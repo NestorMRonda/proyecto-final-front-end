2a77bd823a194bd13015fb13d481f61cca5d83ec
fgronner <alex.casamayor.munoz@gmail.com>
2022-12-15 10:12:56 +0100

    get by email -> GET http://localhost:3000/api/users/email/(email)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { StudentsService } from 'src/app/services/students.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -10,13 +12,15 @@ import { StudentsService } from 'src/app/services/students.service';
 export class LoginComponent {
 
   formulario: FormGroup;
+  token: string;
 
-  constructor(private studentsService: StudentsService) {
+  constructor(private studentsService: StudentsService, private router: Router) {
     this.formulario = new FormGroup({
       email: new FormControl('pepito@gmail.com', [Validators.required]),
       password: new FormControl('test1234', [Validators.required])
       /* Habría que ver si el email y la password coinciden con la base de datos*/
     })
+    this.token ="";
 
   }
 
@@ -24,10 +28,24 @@ export class LoginComponent {
     const response = await this.studentsService.logIn(this.formulario.value)
     /* Response es un objeto con success y el token, el token hay que meterlo en el local storage, habría que meter el id en este objeto para hacer posteriormente el navigate en caso de que sea success */
     if (response.success) {
-      alert('Enhorabuena')
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'El login ha sido correcto',
+        showConfirmButton: false,
+        timer: 1500
+      })
+      this.token = response.token
+      console.log(this.formulario.value)
       /* Meter aquí navigate al perfil del alumno */
     } else {
-      alert(response.fatal)
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'El usuario o contraseña es incorrecto',
+        showConfirmButton: false,
+        timer: 1500
+      })
     }
     this.formulario.reset()
   }
